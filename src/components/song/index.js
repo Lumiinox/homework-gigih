30a59b44fe6554eacc0a7c8a5e993dbab87b9fc7
Lumiinox <maximillian.michael2000@gmail.com>
2022-03-31 11:35:43 +0700

    Added using useState

--- src/components/song/index.js
+++ src/components/song/index.js
@@ -2,13 +2,15 @@ import './index.css';
 
 const Songs = (props) => {
     return(
-        <tr>
-            <td><img className="songImage" src={props.url} /></td>
-            <td className='textTdElement'>{props.name}</td>
-            <td className='textTdElement'>{props.artistName}</td>
-            <td className='textTdElement'>{props.albumName}</td>
-            <td><button type="button">Select</button></td>
-        </tr>
+        <>
+            <tr>
+                <td><img className="songImage" src={props.url} /></td>
+                <td className='textTdElement'>{props.name}</td>
+                <td className='textTdElement'>{props.artistName}</td>
+                <td className='textTdElement'>{props.albumName}</td>
+                <td><button type="button">Select</button></td>
+            </tr>
+        </>
     )
 }
 
--- src/pages/PlayList.js
+++ src/pages/PlayList.js
@@ -1,27 +1,22 @@
-import data from '../data/SpotifyData';
 import Songs from '../components/song/index';
-import Search from '../components/search/index';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import './PlayList.css';
 
 function PlayList(){
-    const spotifySearch = new Search();
-
     const CLIENT_ID = "50617af7a91f49b78dd47bcc7ee69433";
     const REDIRECT_URI = "http://localhost:3000"
     const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
     const RESPONSE_TYPE = "token"
 
-    // const songList = data.map((data, id) => 
-    //  <Songs  key={id} url={data.album.images[2].url} name={data.name} artistName={data.album.artists[0].name} albumName = {data.album.name}/>
-    // );
+    const [token, setToken] = useState("");
+    const [songResult, setResult] = useState([]);
+    const [searchKeyword, setSearchKey] = useState("");
 
-    const renderResult = () => {
-        console.log("TESTING");
-        return spotifySearch.state.result.map(data => (
-            <div key={data.album.id}><img src={data.album.images[2].url}></img></div>
-            
-        ))
+    const RenderSongResult = () => {
+        return songResult.map((data,id) => 
+                <Songs  key={id} url={data.album.images[2].url} name={data.name} artistName={data.album.artists[0].name} albumName = {data.album.name}/>
+        )
     }
 
     useEffect(() => {
@@ -33,45 +28,57 @@ function PlayList(){
     
             window.location.hash = "";
             console.log("URI change");
-            window.localStorage.setItem("token", tokenIn);
+            
+            window.localStorage.setItem("token", tokenIn)
         }
-        console.log(tokenIn);
-        spotifySearch.receiveToken(tokenIn);
-        console.log(spotifySearch.state);
-      })
+        console.log(`tokenIn ${tokenIn}`)
+        setToken(tokenIn)
+        console.log(`token ${token}`)
+      },[])
+
+    const CallSpotifySearch = (e) => {
+        e.preventDefault();
+        console.log(`token in search ${token}`)
+        console.log(`searchKey in search ${searchKeyword}`)
+        axios.get(`https://api.spotify.com/v1/search`,
+        {
+            headers: {
+                Authorization: `Bearer ${token}`
+            },
+            params: {
+                q: searchKeyword,
+                type: "track"
+            }
+        }).then((response)=>{
+            const searchResult = response.data.tracks.items;
+            console.log(searchResult);
+            if(response){
+                setResult(searchResult);
+            }
+        }).catch((e) => console.log(e));
+    }
 
     const logout = () => {
-        spotifySearch.clearToken();
+        setToken("");
         window.localStorage.removeItem("token");
     }  
 
     return(
         <div className='bodyWrapper'>
-            
-            <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Login to Spotify</a>
-            <button onClick={logout}>Logout</button>
-
-            <form onSubmit={spotifySearch.searchCall}>
-                <input type="text" onChange={event => spotifySearch.receiveSearchKey(event.target.value)}/>
+            <h1>Create Playlist</h1>
+            <form onSubmit={CallSpotifySearch}>
+                <input type="text" placeholder='Search..' onChange={e => setSearchKey(e.target.value)}></input>
                 <button type={"submit"}>Search</button>
             </form>
-            
-            {renderResult()}
-{/* 
-            <h1>Create Playlist</h1>
-            <div className="content">
-                <table>
-                    <tbody>
-                        <tr>
-                            <td>Image</td>
-                            <td>Title</td>
-                            <td>Artist</td>
-                            <td>Album</td>
-                            <td></td>
-                        </tr>
-                    </tbody>
-                </table>     
-            </div> */}
+            {!token ?
+                    <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Login
+                        to Spotify</a>
+                    : <button onClick={logout}>Logout</button>}
+            <table>
+                <tbody>
+                    {RenderSongResult()}
+                </tbody>
+            </table>
         </div>
     )
 }
